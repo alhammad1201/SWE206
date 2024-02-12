1af08b68843b2b9eb2fbc883d33c24e1cab5593e
alhammad1201 <alhammad0800@gmail.com>
2024-02-12 18:20:19 +0300

    selectionSort

--- src/Sorter.java
+++ src/Sorter.java
@@ -19,6 +19,21 @@ public class Sorter {
     }
 
     public static void sort(ArrayList<String> students){
-
+        int n = students.size();
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (students.get(j).compareTo(students.get(minIndex)) < 0) {
+                    minIndex = j;
+                }
+            }
+            String temp = students.get(minIndex);
+            students.set(minIndex, students.get(i));
+            students.set(i, temp);
+        }
+        System.out.println("Selection Sorted List:");
+        for (String student : students) {
+            System.out.println(student);
+        }
     }
 }
